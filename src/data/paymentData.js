8a015ac202dc6bac5a02759a0c96c34aa90ad2dd
Bhavasri Krishnamoorthi <bhavasrikrishnamoorthi1979@gmail.com>
2026-06-14 15:54:31 +0530

    Initial React project setup

---
+++ src/data/paymentData.js
@@ -0,0 +1,7 @@
+export const payments = [
+  {
+    id: 1,
+    amount: 25000,
+    status: "Paid",
+  },
+];
---
+++ src/data/userData.js
@@ -0,0 +1,4 @@
+export const user = {
+  name: "Client User",
+  email: "client@test.com",
+};
